f2f918409f12b3d9b590d178b96c5b20f7256997
Sravan <s@o.com>
2017-04-08 14:39:09 +0530

    First publish

---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app/components/app/app.component.html',
+  styleUrls: ['./app/components/app/app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
